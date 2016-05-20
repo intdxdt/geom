ecf67a61d702bc8934b20e85106cad551e243dd9
Titus Tienaah <dxtitus@gmail.com>
2016-05-20 18:35:25 -0300

    udated for struct item

--- line_build.go
+++ line_build.go
@@ -9,7 +9,7 @@ func (self *LineString) build_index() {
     if !self.index.IsEmpty() {
         self.index.Clear()
     }
-    data := make([]rtree.Item, len(self.chains))
+    data := make([]rtree.BoxObj, len(self.chains))
     for i := range self.chains {
         data[i] = self.chains[i]
     }
