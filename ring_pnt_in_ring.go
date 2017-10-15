ed6ef341146478d87de70acf6cd16f4ca0fd9b8a
Titus Tienaah <titus.intdxdt@gmail.com>
2017-10-15 07:23:43 -0300

    minor fmt and doc

--- ring_pnt_in_ring.go
+++ ring_pnt_in_ring.go
@@ -6,26 +6,19 @@ import (
 
 //point completely in ring
 func (self *LinearRing) PointCompletelyInRing(pnt *Point) bool {
-	return self.Envelope().IntersectsPoint(pnt[:]) &&
-			self.completely_in_ring(pnt)
+	return self.Envelope().IntersectsPoint(pnt[:]) && self.completely_in_ring(pnt)
 }
 
-/*
- Test whether a point lies inside a ring.
- The ring may be oriented in either direction.
- If the point lies on the ring boundary the result of this method is unspecified.
- This algorithm does not attempt to first check the point against the envelope
- of the ring.
- param p{Point} point to check for ring inclusion
- param ring{LinearRing} assumed to have first point identical to last point
- return {boolean}
-*/
+ //Test whether a point lies inside a ring.
+ //The ring may be oriented in either direction.
+ //If the point lies on the ring boundary the result of this method is unspecified.
+ //This algorithm does not attempt to first check the point against the envelope of the ring.
 func (self *LinearRing) completely_in_ring(pnt *Point) bool {
 	var i, i1 int
 	var p1, p2 *Point
 	var x1, y1, x2, y2, xInt float64
 	var p = *pnt
-	// For each segment l = (i-1, i), see if it crosses ray from test point in positive x direction.
+	// for each segment l = (i-1, i), see if it crosses ray from test point in positive x direction.
 	var crossings = 0 // number of segment/ray crossings
 	for i = 1; i < self.LenVertices(); i++ {
 		i1 = i - 1
@@ -38,7 +31,7 @@ func (self *LinearRing) completely_in_ring(pnt *Point) bool {
 			//segment straddles x axis, so compute intersection with x-axis.
 			xInt = float64(math.SignOfDet2(x1, y1, x2, y2)) / (y2 - y1)
 			//xsave = xInt
-			//  crosses ray if strictly positive intersection.
+			//crosses ray if strictly positive intersection.
 			if xInt > 0.0 {
 				crossings++
 			}
